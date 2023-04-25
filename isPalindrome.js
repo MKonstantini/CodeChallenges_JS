bf82865114811d1064efca3ff23afce559b83366
Matan Konstantini <106117669+MKonstantini@users.noreply.github.com>
2023-04-25 13:19:24 +0300

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,6 +1,7 @@
 // Function that checks if num is a palindrome //
 
 function CheckPalindrome(num) {
+    //Break digits into an array:
     let draftNum = num;
     let arrayNum1 = [];
     while(draftNum > 0) {
@@ -8,9 +9,11 @@ function CheckPalindrome(num) {
         arrayNum1.push(x);
         draftNum = Math.floor(draftNum / 10);
     }
+    //Duplicate and reverse array:
     let arrayNum2 = arrayNum1.slice();
     arrayNum1.reverse();
-
+    
+    //Check if arrays match each other:
     let isPalindrome = true;
     for (let i = 0; i < arrayNum1.length / 2; i++) {
         if (arrayNum1[i] != arrayNum2[i]) {
