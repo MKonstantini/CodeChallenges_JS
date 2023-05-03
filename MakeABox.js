ea80374ebb0dedef2931dd6d797ee6de1c1c753a
Matan Konstantini <106117669+MKonstantini@users.noreply.github.com>
2023-05-03 18:22:40 +0300

    Add files via upload

---
+++ MakeABox.js
@@ -0,0 +1,31 @@
+//Write a function that makes a box the length of given num
+
+function makeBox(num) {
+    const box = [];
+    
+    //loop within loop for square, length = num:
+    for (let i = 0; i < num; i++) {
+        const row = [];
+        for (let j = 0; j < num; j++) {
+            //push '#' for the four edges:
+            if ([i, j, i+1 - num, j+1 - num].includes(0)) 
+            row.push("#");
+
+            //else push a blank (' '):
+            else 
+            row.push(" ");
+        }
+
+        box.push(row);
+    }
+    
+    return box;
+}    
+
+/* Example - makeBox(5) :
+
+    #####
+    #   #
+    #   #
+    #####
+*/
